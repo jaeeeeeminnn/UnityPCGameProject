498f7ea3b18fe51c5327860506ffa4d1a2c70636
jaeeeeeminnn <jj5488jj@naver.com>
2022-10-15 21:07:03 +0900

    캐릭터 이동 구현
    캐릭터 이동 구현

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float moveX;
+
+    Rigidbody2D rb;
+
+    [Header("이동 속도")]
+    [SerializeField] [Range(100f, 2000f)] float moveSpeed = 200f;
+
+    [Header("점프 강도")]
+    [SerializeField] [Range(100f, 800f)] float jumpForce = 400f;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update");
+        moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        rb.velocity = new Vector2(moveX, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(rb.velocity.y == 0)
+            {
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
+            }
+        }
     }
 }
