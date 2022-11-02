f4e19d9175fd5e42de0ddcbf506ca57a2d62327a
jaeeeeeminnn <jj5488jj@naver.com>
2022-11-02 23:12:14 +0900

    튜토리얼 개발 중

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public int stageIndex;
+    public static GameManager instance;
+
+    public int stageIndex = -1;
+
+    public PlayerController playerController;
+    public GameObject informationText;
 
     // Start is called before the first frame update
     void Start()
     {
-        stageIndex = 0;
+        instance = this;
+
+        if(stageIndex == -1)
+        {
+            Tutorial();
+            //StartCoroutine("TutorialRoutine");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //playerController.enabled = false;
+        //informationText.SetActive(true);
+        //playerController.enabled = true;
+        //if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        //{
+            //informationText.SetActive(false);
+        //}
     }
 
     public void NextStage()
     {
         stageIndex++;
+    }
+
+    //IEnumerator TutorialRoutine()
+    //{
+        //playerController.enabled = false;
+    //}
+
+    public void Tutorial()
+    {
+        Debug.Log("튜토리얼 진입함");
+        playerController.enabled = false;
+        informationText.SetActive(true);
+        playerController.enabled = true;
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            informationText.SetActive(false);
+        }
     }
 }
