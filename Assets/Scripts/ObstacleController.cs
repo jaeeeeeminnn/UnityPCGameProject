08b664beab748e04dce7033c79c56c5314582ef1
jaeeeeeminnn <92133365+jaeeeeeminnn@users.noreply.github.com>
2022-11-20 16:59:26 +0900

    ObstacleController Scirpt 수정

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ObstacleController : MonoBehaviour
 {
+    public Tilemap map;
+    //public TileBase tilebase;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        map.GetTile(new Vector3Int(0, 0, 0));
     }
 
     // Update is called once per frame
