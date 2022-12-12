8f571be7595a71400042fe70a4476cedf3861b5c
jaeeeeeminnn <jj5488jj@naver.com>
2022-12-12 20:13:16 +0900

    Stage1 구성

--- Assets/Scripts/Stage1ObController.cs
+++ Assets/Scripts/Stage1ObController.cs
@@ -1,17 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
-public class ObstacleController : MonoBehaviour
+public class Stage1ObController : MonoBehaviour
 {
-    public Tilemap map;
-    //public TileBase tilebase;
-
     // Start is called before the first frame update
     void Start()
     {
-        map.GetTile(new Vector3Int(0, 0, 0));
+        
     }
 
     // Update is called once per frame
