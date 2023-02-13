0dd3e68aab7ad69fa8e0a73c1ea173c2f5e9aba7
jaeeeeeminnn <jj5488jj@naver.com>
2023-02-13 23:35:43 +0900

    Data.cs에 Vector3 savePoint 추가

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,11 +1,12 @@
 using System;
+using UnityEngine;
 
 [Serializable] // Á÷·ÄÈ­
 public class Data
 {
     public bool[] isUnlock = new bool[10];
     public int deathCount;
-    //Transform savePoint;
+    public Vector3 savePoint;
 
     private void Start()
     {
